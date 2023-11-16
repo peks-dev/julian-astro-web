bc7e31478e8fb0777e800f3962a480b24ec16d9f
Fernando Montes <peks@criptext.com>
2023-11-16 02:41:16 -0600

    Upgrade imgs quality

--- src/context/projectsData.js
+++ src/context/projectsData.js
@@ -4,36 +4,36 @@ export const projectsData = [
     description: "julian collazo bass project",
     category: "short film",
     url: "/commercial/BeatnikReebok",
-    coverPath: "/projects/commercial/beatnik-reebok/beatnik_reebok_7.webp",
+    coverPath: "/projects/commercial/beatnik-reebok/beatnik_reebok_7.jpeg",
     metaImage: "/projects/commercial/beatnik-reebok/beatnik_reebok_meta.jpg",
     images: [
       {
-        src: "/projects/commercial/beatnik-reebok/beatnik_reebok_1.webp",
+        src: "/projects/commercial/beatnik-reebok/beatnik_reebok_7.jpeg",
+        alt: "beatnik reebok image 7",
+      },
+      {
+        src: "/projects/commercial/beatnik-reebok/beatnik_reebok_1.jpg",
         alt: "beatnik reebok image 1",
       },
       {
-        src: "/projects/commercial/beatnik-reebok/beatnik_reebok_2.webp",
-        alt: "beatnik reebok image 2",
-      },
-      {
-        src: "/projects/commercial/beatnik-reebok/beatnik_reebok_3.webp",
+        src: "/projects/commercial/beatnik-reebok/beatnik_reebok_3.jpg",
         alt: "beatnik reebok image 3",
       },
       {
-        src: "/projects/commercial/beatnik-reebok/beatnik_reebok_4.webp",
-        alt: "beatnik reebok image 4",
+        src: "/projects/commercial/beatnik-reebok/beatnik_reebok_2.jpg",
+        alt: "beatnik reebok image 2",
       },
       {
-        src: "/projects/commercial/beatnik-reebok/beatnik_reebok_5.webp",
+        src: "/projects/commercial/beatnik-reebok/beatnik_reebok_5.jpg",
         alt: "beatnik reebok image 5",
       },
       {
-        src: "/projects/commercial/beatnik-reebok/beatnik_reebok_6.webp",
+        src: "/projects/commercial/beatnik-reebok/beatnik_reebok_6.jpg",
         alt: "beatnik reebok image 6",
       },
       {
-        src: "/projects/commercial/beatnik-reebok/beatnik_reebok_7.webp",
-        alt: "beatnik reebok image 7",
+        src: "/projects/commercial/beatnik-reebok/beatnik_reebok_4.jpg",
+        alt: "beatnik reebok image 4",
       },
     ],
     teamMates: {
@@ -47,35 +47,35 @@ export const projectsData = [
     description: "julian collazo bass project",
     category: "short film",
     url: "/commercial/ModPizza",
-    coverPath: "/projects/commercial/mod-pizza/mod_pizza_4.webp",
+    coverPath: "/projects/commercial/mod-pizza/mod_pizza_4.jpeg",
     metaImage: "/projects/commercial/mod-pizza/mod_pizza_meta.jpg",
     images: [
       {
-        src: "/projects/commercial/mod-pizza/mod_pizza_4.webp",
+        src: "/projects/commercial/mod-pizza/mod_pizza_4.jpeg",
         alt: "mod pizza image 4",
       },
       {
-        src: "/projects/commercial/mod-pizza/mod_pizza_6.webp",
+        src: "/projects/commercial/mod-pizza/mod_pizza_6.jpeg",
         alt: "mod pizza image 6",
       },
       {
-        src: "/projects/commercial/mod-pizza/mod_pizza_3.webp",
+        src: "/projects/commercial/mod-pizza/mod_pizza_3.jpeg",
         alt: "mod pizza image 3",
       },
       {
-        src: "/projects/commercial/mod-pizza/mod_pizza_7.webp",
+        src: "/projects/commercial/mod-pizza/mod_pizza_7.jpeg",
         alt: "mod pizza image 7",
       },
       {
-        src: "/projects/commercial/mod-pizza/mod_pizza_1.webp",
+        src: "/projects/commercial/mod-pizza/mod_pizza_1.jpeg",
         alt: "mod pizza image 1",
       },
       {
-        src: "/projects/commercial/mod-pizza/mod_pizza_5.webp",
+        src: "/projects/commercial/mod-pizza/mod_pizza_5.jpeg",
         alt: "mod pizza image 5",
       },
       {
-        src: "/projects/commercial/mod-pizza/mod_pizza_2.webp",
+        src: "/projects/commercial/mod-pizza/mod_pizza_2.jpeg",
         alt: "mod pizza image 2",
       },
     ],
@@ -90,35 +90,35 @@ export const projectsData = [
     description: "julian collazo bass project",
     category: "short film",
     url: "/narrative/ChocolateCake",
-    coverPath: "/projects/narrative/chocolate-cake/chocolate_cake_1.webp",
+    coverPath: "/projects/narrative/chocolate-cake/chocolate_cake_1.png",
     metaImage: "/projects/narrative/chocolate-cake/chocolate_cake_meta.jpg",
     images: [
       {
-        src: "/projects/narrative/chocolate-cake/chocolate_cake_1.webp",
+        src: "/projects/narrative/chocolate-cake/chocolate_cake_1.png",
         alt: "chocolate cake image 1",
       },
       {
-        src: "/projects/narrative/chocolate-cake/chocolate_cake_2.webp",
+        src: "/projects/narrative/chocolate-cake/chocolate_cake_2.png",
         alt: "chocolate cake image 2",
       },
       {
-        src: "/projects/narrative/chocolate-cake/chocolate_cake_3.webp",
+        src: "/projects/narrative/chocolate-cake/chocolate_cake_3.png",
         alt: "chocolate cake image 3",
       },
       {
-        src: "/projects/narrative/chocolate-cake/chocolate_cake_4.webp",
+        src: "/projects/narrative/chocolate-cake/chocolate_cake_4.png",
         alt: "chocolate cake image 4",
       },
       {
-        src: "/projects/narrative/chocolate-cake/chocolate_cake_5.webp",
-        alt: "chocolate cake image 5",
-      },
-      {
-        src: "/projects/narrative/chocolate-cake/chocolate_cake_6.webp",
+        src: "/projects/narrative/chocolate-cake/chocolate_cake_6.png",
         alt: "chocolate cake image 6",
       },
       {
-        src: "/projects/narrative/chocolate-cake/chocolate_cake_7.webp",
+        src: "/projects/narrative/chocolate-cake/chocolate_cake_5.png",
+        alt: "chocolate cake image 5",
+      },
+      {
+        src: "/projects/narrative/chocolate-cake/chocolate_cake_7.png",
         alt: "chocolate cake image 7",
       },
     ],
@@ -133,32 +133,36 @@ export const projectsData = [
     description: "julian collazo bass project",
     category: "short film",
     url: "/narrative/FaceValues",
-    coverPath: "/projects/narrative/face-values/face_value_1.webp",
+    coverPath: "/projects/narrative/face-values/face_value_1.jpeg",
     metaImage: "/projects/narrative/face-values/face_value_meta.jpg",
     images: [
       {
-        src: "/projects/narrative/face-values/face_value_1.webp",
+        src: "/projects/narrative/face-values/face_value_1.jpeg",
         alt: "face value image 1",
+      },
+      {
+        src: "/projects/narrative/face-values/face_value_4.png",
+        alt: "face value image 4",
+      },
+      {
+        src: "/projects/narrative/face-values/face_value_6.png",
+        alt: "face value image 6",
+      },
+      {
+        src: "/projects/narrative/face-values/face_value_3.png",
+        alt: "face value image 3",
+      },
+      {
+        src: "/projects/narrative/face-values/face_value_7.png",
+        alt: "face value image 7",
       },
       {
         src: "/projects/narrative/face-values/face_value_2.webp",
         alt: "face value image 2",
       },
       {
-        src: "/projects/narrative/face-values/face_value_3.webp",
-        alt: "face value image 3",
-      },
-      {
-        src: "/projects/narrative/face-values/face_value_4.webp",
-        alt: "face value image 4",
-      },
-      {
-        src: "/projects/narrative/face-values/face_value_5.webp",
+        src: "/projects/narrative/face-values/face_value_5.png",
         alt: "face value image 5",
-      },
-      {
-        src: "/projects/narrative/face-values/face_value_6.webp",
-        alt: "face value image 6",
       },
     ],
     teamMates: {
@@ -172,16 +176,16 @@ export const projectsData = [
     description: "julian collazo bass project",
     category: "short film",
     url: "/narrative/Lily",
-    coverPath: "/projects/narrative/lily/lily_1.webp",
+    coverPath: "/projects/narrative/lily/lily_7.jpeg",
     metaImage: "/projects/narrative/lily/lily_meta.jpg",
     images: [
-      { src: "/projects/narrative/lily/lily_1.webp", alt: "lily image 1" },
-      { src: "/projects/narrative/lily/lily_2.webp", alt: "lily image 2" },
-      { src: "/projects/narrative/lily/lily_3.webp", alt: "lily image 3" },
-      { src: "/projects/narrative/lily/lily_4.webp", alt: "lily image 4" },
-      { src: "/projects/narrative/lily/lily_5.webp", alt: "lily image 5" },
-      { src: "/projects/narrative/lily/lily_6.webp", alt: "lily image 6" },
-      { src: "/projects/narrative/lily/lily_7.webp", alt: "lily image 7" },
+      { src: "/projects/narrative/lily/lily_7.jpeg", alt: "lily image 7" },
+      { src: "/projects/narrative/lily/lily_1.jpeg", alt: "lily image 1" },
+      { src: "/projects/narrative/lily/lily_2.jpeg", alt: "lily image 2" },
+      { src: "/projects/narrative/lily/lily_3.jpeg", alt: "lily image 3" },
+      { src: "/projects/narrative/lily/lily_4.jpeg", alt: "lily image 4" },
+      { src: "/projects/narrative/lily/lily_5.jpeg", alt: "lily image 5" },
+      { src: "/projects/narrative/lily/lily_6.jpeg", alt: "lily image 6" },
     ],
     teamMates: {
       dir: "person dir",
@@ -194,13 +198,13 @@ export const projectsData = [
     description: "julian collazo bass project",
     category: "short film",
     url: "/narrative/Ptsd",
-    coverPath: "/projects/narrative/ptsd/ptsd_1.webp",
+    coverPath: "/projects/narrative/ptsd/ptsd_4.png",
     metaImage: "projects/narrative/ptsd/ptsd_meta.jpg",
     images: [
-      { src: "/projects/narrative/ptsd/ptsd_1.webp", alt: "ptsd image 1" },
-      { src: "/projects/narrative/ptsd/ptsd_2.webp", alt: "ptsd image 2" },
-      { src: "/projects/narrative/ptsd/ptsd_3.webp", alt: "ptsd image 3" },
-      { src: "/projects/narrative/ptsd/ptsd_4.webp", alt: "ptsd image 4" },
+      { src: "/projects/narrative/ptsd/ptsd_4.png", alt: "ptsd image 4" },
+      { src: "/projects/narrative/ptsd/ptsd_2.jpeg", alt: "ptsd image 2" },
+      { src: "/projects/narrative/ptsd/ptsd_1.jpeg", alt: "ptsd image 1" },
+      { src: "/projects/narrative/ptsd/ptsd_3.jpeg", alt: "ptsd image 3" },
     ],
     teamMates: {
       dir: "person dir",
